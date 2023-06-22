c55a2feed01f99f57e091edaed95c40b9b407168
winsofttutorials <winsofttutorials@gmail.com>
2023-06-22 21:03:50 +0100

    'projectFIles'

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import "./Hero.css";
+import { logo } from "../../components";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section className="h-wrapper">
+        <div className="h-container">
+          <nav>
+            <img src={logo} alt="logo" className="logo" />
+            <div id="navBar">
+              <ul>
+                <li>
+                  <a href="/">Features</a>
+                </li>
+                <li>
+                  <a href="/">Pricing</a>
+                </li>
+                <li>
+                  <a href="/">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <button>Login</button>
+          </nav>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Features } from "./Features/Features";
 export { default as Download } from "./Download/Download";
 export { default as Faq } from "./Faq/Faq";
 export { default as Footer } from "./Footer/Footer";
+export { default as GetStarted } from "./GetStarted/GetStarted";
+export { default as logo } from "../images/logo-bookmark.svg";
